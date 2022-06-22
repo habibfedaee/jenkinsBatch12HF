842955068f98a7b33863ca43bf6cfd12148ec8a8
habibfedaee <habib.fedaee@gmail.com>
2022-06-21 18:42:11 -0700

    dog sout added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i=0 ;i<5 ;i++){
             System.out.println("hello! am i a cat?");
+            System.out.println("hello, you might be a dog!");
         }
     }
 }
