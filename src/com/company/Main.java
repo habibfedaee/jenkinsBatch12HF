def1136a610b3e1e5e77b3c67a1b45cdb37801ac
habibfedaee <habib.fedaee@gmail.com>
2022-06-21 18:08:23 -0700

    first commit for jenkins

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=0 ;i<5 ;i++){
+            System.out.println("hello! am i a cat?");
+        }
+    }
+}
